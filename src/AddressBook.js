5ece09e0fa6649751587e084dd7b592143c8dcba
IlonaKolo <ilona.kolossova@khk.ee>
2019-04-12 00:39:24 +0300

    Working on fails

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,23 +1,27 @@
-function Addresbook() {
-	this.contacts = [];
-	this.initialsComplete = false;
-} 
-
-AddressBook.prototype.getInitalContacts = function(cb) {
-	var self = this;
-	
-	setTimeout(function() {
-		self.initalComplete = true;
-		if (cb) {
-			return cb();
-		}
-	}, 3);
+function AddressBook() {
+  this.contacts = [];
+  this.initialComplete = false;
 }
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+  var self = this;
+
+  setTimeout(function() {
+    self.initialComplete = true;
+    if (cb) {
+      return cb();
+    }
+  }, 3);
+};
 
 AddressBook.prototype.addContact = function(contact) {
-	this.contacts.push(contact);
-}
+  this.contacts.push(contact);
+};
 
 AddressBook.prototype.getContact = function(index) {
-	return this.contacts[index];
-}
+  return this.contacts[index];
+};
+
+AddressBook.prototype.deleteContact = function(index) {
+  return this.splice(index, 1)
+};
--- src/Contact.js
+++ src/Contact.js
@@ -1,3 +1,1 @@
-function Contact() {
-	
-}
+function Contact() {}
